757c096c63572d61c4eb29946691131248997544
s1300038 <s1300038@u-aizu.ac.jp>
2023-06-15 19:46:18 +0900

    Greetings added

--- dicegame.java
+++ dicegame.java
@@ -1,7 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class dicegame{
     public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("What is your name? ");
+	String c = sc.nextLine();
+	System.out.println("Hello, " + c + "!");
 	System.out.println("Rolling dice...");
 
 	Random random = new Random();
@@ -13,5 +18,7 @@ public class dicegame{
 	System.out.println("Die 1: " + a);
 	System.out.println("Die 2: " + b);
 	System.out.println("Total value: " + d);
+
+	scanner.close();
     }
 }
